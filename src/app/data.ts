c9ae24cdb8101321273a12602ebb8d894b6395aa
Rhushikesh Lokhande <rhushikeshl@byteridge.com>
2018-07-18 09:44:13 +0530

    test

--- src/app/data.ts
+++ src/app/data.ts
@@ -29,17 +29,17 @@ export const Data = {
         }
     ],
     bannerImages: [
-        'https://scontent.fhyd6-1.fna.fbcdn.net/v/t1.0-9/36518342_1853704868019794_7607919684461002752_n.jpg?_nc_cat=0&oh=66e00b8b4aa1093dd4b3b811ce962153&oe=5BDDD721',
-        'https://scontent.fhyd6-1.fna.fbcdn.net/v/t1.0-9/36463607_1853544891369125_1243007262524440576_n.jpg?_nc_cat=0&oh=fc73d96fcfe85f0fd79ddf149c4e545f&oe=5BDF7BB6',
-        'https://scontent.fhyd6-1.fna.fbcdn.net/v/t1.0-9/36489798_1853545034702444_9215454943249432576_n.jpg?_nc_cat=0&oh=92a094ab014c67f13015513c6715b8b0&oe=5BA9E18C',
-        'https://scontent.fhyd6-1.fna.fbcdn.net/v/t1.0-9/36352710_1851329094924038_1722458069549449216_n.jpg?_nc_cat=0&oh=13c4bb2473fbb6c955492804c16c19c4&oe=5BDB35B6',
-        'https://scontent.fhyd6-1.fna.fbcdn.net/v/t1.0-9/36325619_1848618135195134_6442160733059809280_n.jpg?_nc_cat=0&oh=e2f0883e721ac16fea40a03d036ec015&oe=5BE36410',
+        'https://scontent.fhyd6-1.fna.fbcdn.net/v/t1.0-9/28056112_2028089947465499_4661486433122095559_n.jpg?_nc_cat=0&oh=8b31ca0621dd3349c449651d1e2f3589&oe=5BE45876',
+        'https://scontent.fhyd6-1.fna.fbcdn.net/v/t1.0-9/25298994_1997515213856306_565915211209062134_n.jpg?_nc_cat=0&oh=b853f7ec02058de26ac188902fefacc6&oe=5BE3CBAA',
+        'https://scontent.fhyd6-1.fna.fbcdn.net/v/t1.0-9/23658901_1507652215992429_8596601057313673736_n.jpg?_nc_cat=0&oh=e4dfd2a95169dd4d2cfe99a21c532f53&oe=5BE9E966',
+        'https://scontent.fhyd6-1.fna.fbcdn.net/v/t1.0-9/15109440_395776777478475_4412434734155623418_n.jpg?_nc_cat=0&oh=4f0f0c31d0ce6bf9973b695ed25d2de6&oe=5BD9006E',
+        'https://scontent.fhyd6-1.fna.fbcdn.net/v/t1.0-9/13921171_1052859661477403_8838082226073301756_n.jpg?_nc_cat=0&oh=3ed0ee9cdb6f6b02bebd09605844db77&oe=5BD06E8C'
     ],
-    name: 'Vivek Sahu',
-    post: 'Member of legislative assembly (MLA) , Karanja',
-    profilePic: 'https://scontent.fhyd6-1.fna.fbcdn.net/v/t1.0-9/1535733_639640026092957_1870388539_n.jpg?_nc_cat=0&oh=87af762dfcef63de23942e49bb418765&oe=5BD950F8',
+    name: 'Prashant Ikhar',
+    post: 'Vice President at Santaji Yuva Morcha',
+    profilePic: 'https://scontent.fhyd6-1.fna.fbcdn.net/v/t1.0-9/22894301_682401305279192_5921079090309140975_n.jpg?_nc_cat=0&oh=311b14540ce0ed9781e8afc795872cb5&oe=5BD8B3E5',
     profileDetails: `
-WELCOME to my website <b>www.temp.com</b></br></br>
+WELCOME to my website <b>www.prashant.ikhar.com</b></br></br>
 <p>My only purpose of building this website is to make complete use of the technology we have at our disposal to connect in an efficient manner with you all and I consider this as a source of 2 way interaction directly between you and me. Effort will be to constantly keep you updated and stay in touch.</p>
 
 <p>Soon information regarding my schedule will be added and constantly updated to help you know where and when I would be. This will help overcome the inconvenience I have seen people face who want to meet me and know about my location.</p>
@@ -71,16 +71,11 @@ WELCOME to my website <b>www.temp.com</b></br></br>
             name: 'Bhushand dhod'
         }
     ],
-    galleryImages: [
-        'https://scontent.fhyd6-1.fna.fbcdn.net/v/t1.0-9/36518342_1853704868019794_7607919684461002752_n.jpg?_nc_cat=0&oh=66e00b8b4aa1093dd4b3b811ce962153&oe=5BDDD721',
-        'https://scontent.fhyd6-1.fna.fbcdn.net/v/t1.0-9/36463607_1853544891369125_1243007262524440576_n.jpg?_nc_cat=0&oh=fc73d96fcfe85f0fd79ddf149c4e545f&oe=5BDF7BB6',
-        'https://scontent.fhyd6-1.fna.fbcdn.net/v/t1.0-9/36489798_1853545034702444_9215454943249432576_n.jpg?_nc_cat=0&oh=92a094ab014c67f13015513c6715b8b0&oe=5BA9E18C',
-        'https://scontent.fhyd6-1.fna.fbcdn.net/v/t1.0-9/36352710_1851329094924038_1722458069549449216_n.jpg?_nc_cat=0&oh=13c4bb2473fbb6c955492804c16c19c4&oe=5BDB35B6',
-        'https://scontent.fhyd6-1.fna.fbcdn.net/v/t1.0-9/36325619_1848618135195134_6442160733059809280_n.jpg?_nc_cat=0&oh=e2f0883e721ac16fea40a03d036ec015&oe=5BE36410',
-        'https://scontent.fhyd6-1.fna.fbcdn.net/v/t1.0-9/36518342_1853704868019794_7607919684461002752_n.jpg?_nc_cat=0&oh=66e00b8b4aa1093dd4b3b811ce962153&oe=5BDDD721',
-        'https://scontent.fhyd6-1.fna.fbcdn.net/v/t1.0-9/36463607_1853544891369125_1243007262524440576_n.jpg?_nc_cat=0&oh=fc73d96fcfe85f0fd79ddf149c4e545f&oe=5BDF7BB6',
-        'https://scontent.fhyd6-1.fna.fbcdn.net/v/t1.0-9/36489798_1853545034702444_9215454943249432576_n.jpg?_nc_cat=0&oh=92a094ab014c67f13015513c6715b8b0&oe=5BA9E18C',
-        'https://scontent.fhyd6-1.fna.fbcdn.net/v/t1.0-9/36352710_1851329094924038_1722458069549449216_n.jpg?_nc_cat=0&oh=13c4bb2473fbb6c955492804c16c19c4&oe=5BDB35B6',
-        'https://scontent.fhyd6-1.fna.fbcdn.net/v/t1.0-9/36325619_1848618135195134_6442160733059809280_n.jpg?_nc_cat=0&oh=e2f0883e721ac16fea40a03d036ec015&oe=5BE36410',
+    galleryImages:  [
+        'https://scontent.fhyd6-1.fna.fbcdn.net/v/t1.0-9/28056112_2028089947465499_4661486433122095559_n.jpg?_nc_cat=0&oh=8b31ca0621dd3349c449651d1e2f3589&oe=5BE45876',
+        'https://scontent.fhyd6-1.fna.fbcdn.net/v/t1.0-9/25298994_1997515213856306_565915211209062134_n.jpg?_nc_cat=0&oh=b853f7ec02058de26ac188902fefacc6&oe=5BE3CBAA',
+        'https://scontent.fhyd6-1.fna.fbcdn.net/v/t1.0-9/23658901_1507652215992429_8596601057313673736_n.jpg?_nc_cat=0&oh=e4dfd2a95169dd4d2cfe99a21c532f53&oe=5BE9E966',
+        'https://scontent.fhyd6-1.fna.fbcdn.net/v/t1.0-9/15109440_395776777478475_4412434734155623418_n.jpg?_nc_cat=0&oh=4f0f0c31d0ce6bf9973b695ed25d2de6&oe=5BD9006E',
+        'https://scontent.fhyd6-1.fna.fbcdn.net/v/t1.0-9/13921171_1052859661477403_8838082226073301756_n.jpg?_nc_cat=0&oh=3ed0ee9cdb6f6b02bebd09605844db77&oe=5BD06E8C'
     ],
 };
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,10 +1,18 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Data} from '../data';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
+import { Data } from '../data';
 
 @Component({
     selector: 'app-profile',
     templateUrl: './profile.component.html',
+    styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
+export class ProfileComponent implements AfterViewInit {
     data = Data;
+    ngAfterViewInit() {
+        timeline(document.querySelectorAll('.timeline'), {
+            mode: 'horizontal',
+            forceVerticalMode: 768,
+        });
+    }
+
 }
